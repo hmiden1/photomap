9733b0bef2fadc4c5dbde10a19457668b969454c
hmiden1 <77756911+hmiden1@users.noreply.github.com>
2021-04-16 13:45:37 -0500

    Update photomap.js

--- photomap.js
+++ photomap.js
@@ -14,7 +14,7 @@
  */
 function createPhotoMap () {
   // URL of a Google Sheets spreadsheet output as CSV
-  var csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRHJQb5e_WrsRFmjotGSA-H6AyQ19NjOvU_pIIwzFW6WfoKYlIWc0I6Tdvtzb_DMI5NVzPOkJK8PBUW/pub?gid=0&single=true&output=csv'
+  var csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRHJQb5e_WrsRFmjotGSA-H6AyQ19NjOvU_pIIwzFW6WfoKYlIWc0I6Tdvtzb_DMI5NVzPOkJK8PBUW/pub?gid=0&single=true&output=csv';
   
   // create map object with center lat/lon and zoom level
   var map = L.map('map').setView([30.44, -91.187], 13);
